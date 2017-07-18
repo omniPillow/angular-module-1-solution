8560da7a80cb83240ab4a7fb7b6e1ceea2277b1f
chris.niu <chris@something.com>
2017-07-18 17:16:30 -0400

    Assignment 1 complete: version 1.0 check-in

--- app.js
+++ app.js
@@ -7,11 +7,49 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope','$filter'];
 
 function LunchCheckController($scope){
-  $scope.checkMessage = "something";
-  $scope.lunchItems = "empty";
+  $scope.checkMessage = "";
+  $scope.lunchItems = "";
+  $scope.lunchCheckStyle = {};
+  var redStyle = {
+        'color':'red',
+        'border-style':'solid',
+        'border-color':'red',
+        'display':'inline'
+        };
+  var greenStyle = {
+        'color':'green',
+        'border-style':'solid',
+        'border-color':'green',
+        'display':'inline'
+        };
+  $scope.checkLunchItem = function(){
+    var returnMessage = updateMessage($scope.lunchItems);
+    $scope.checkMessage = returnMessage;
+  }
 
+  function updateMessage(string){
+    var itemArray = [];
+    var itemNum = 0;
+    var returnString = "";
+    itemArray = string.split(',');
+    for(var i = 0; i < itemArray.length; i++){
+      if(itemArray[i].trim()){
+        itemNum++;
+      }
+    }
 
-
+    if(itemNum == 0){
+      returnString = "Please enter data first";
+      $scope.lunchCheckStyle.style =redStyle;
+    }else if((itemNum > 0 )&&(itemNum < 4)){
+      returnString = "Enjoy!"
+      $scope.lunchCheckStyle.style = greenStyle;
+    }else if(itemNum >= 4){
+      returnString = "Too much!"
+      $scope.lunchCheckStyle.style = greenStyle;
+    }
+    return returnString;
+  }
 
 }
 
